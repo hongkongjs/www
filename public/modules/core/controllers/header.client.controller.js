ada2c21eeffb9f287096f748bbe3c2ec493c388d
Victor Leung <victorleungtw@gmail.com>
2015-02-18 19:18:55 +0800

    wire up the authentication

--- public/modules/core/controllers/header.client.controller.js
+++ public/modules/core/controllers/header.client.controller.js
@@ -1,9 +1,11 @@
 'use strict';
 
-angular.module('core').controller('HeaderController', ['$scope', '$mdSidenav',
-  function($scope, $mdSidenav){
+angular.module('core').controller('HeaderController', ['$scope', 'Authentication', '$mdSidenav',
+  function($scope, Authentication, $mdSidenav){
     $scope.toggleSidenav = function(menuId) {
       $mdSidenav(menuId).toggle();
     };
+
+    $scope.authentication = Authentication;
   }
 ]);
