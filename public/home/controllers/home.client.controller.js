cc7a82c46b3953bcc93f0076e94e9a81282c053a
Kevin Bluer <kevin@bluer.com>
2015-04-11 23:48:32 +0800

    adding articles to the hompage

--- public/home/controllers/home.client.controller.js
+++ public/home/controllers/home.client.controller.js
@@ -2,9 +2,13 @@
 'use strict';
 
 // Create the 'example' controller
-angular.module('home').controller('HomeController', ['$scope', 'Authentication',
-  function($scope, Authentication) {
+angular.module('home').controller('HomeController', ['$scope', 'Authentication', 'Articles',
+  function($scope, Authentication, Articles) {
     // Expose the authentication service
     $scope.authentication = Authentication;
+
+   $scope.articles = function() {
+   	$scope.articles = Articles.query();
+   };
   }
 ]);
